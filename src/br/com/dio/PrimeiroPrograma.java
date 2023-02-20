ab35b1a64671d444894e2cf7ed070498976a5c30
EdvaldoFagundesmj <edvaldofagundesmj@gmail.com>
2023-02-20 07:48:19 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,15 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+    /*    int a = 3;
+        int b = 7;
+
+        System.out.println("Hello World é " + (a+b));*/
+
+        Gato gato = new Gato();
+        System.out.println(gato);
+    }
+}
