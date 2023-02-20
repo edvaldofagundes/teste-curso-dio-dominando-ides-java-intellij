1e22a6a596b9cded0b8bf82ada79811217bc08fa
EdvaldoFagundesmj <edvaldofagundesmj@gmail.com>
2023-02-20 08:41:30 -0300

    Calculadora criada

---
+++ src/br/com/dio/calculadora/calculadora.java
@@ -0,0 +1,37 @@
+package br.com.dio.calculadora;
+
+import java.util.Scanner;
+
+public class calculadora {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a, b;
+
+        System.out.println("Digite o primeiro valor");
+        a = scan.nextInt();
+        System.out.println("Digite o segundo valor");
+        b = scan.nextInt();
+
+        int somar = somar(a, b);
+        int subtrair = subtrair(a, b);
+        int multiplicar = multiplicar(a, b);
+        float dividir = dividir(a, b);
+
+        System.out.println("Soma = " + somar);
+        System.out.println("subtra = " + subtrair);
+        System.out.println("multiplicação = " + multiplicar);
+        System.out.println("divisão = " + dividir);
+
+    }
+
+    public static int somar(int a, int b){
+        return a+b;
+    } public static int subtrair(int a, int b){
+        return a-b;
+    }    public static int multiplicar(int a, int b){
+        return a*b;
+    } public static float dividir( float a,  float b){
+        return a/b;
+    }
+
+}
